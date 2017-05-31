1ab64ebc2f710dc153ded9e9730ec46f862d3431
Tiago Vieira <tiagopereiravieira@gmail.com>
2017-05-31 08:40:55 -0300

    Cap11 - Criação de Diretórios

---
+++ src/cap11/CriaDiretorio.java
@@ -0,0 +1,58 @@
+
+package cap11;
+
+import java.io.File;
+
+/**
+ *
+ * @author Login
+ */
+public class CriaDiretorio {
+    
+    public static void main(String[] args) {
+        
+        File dir = new File("c:/loja");
+        
+        if (!dir.exists()) {
+            
+            dir.mkdir();
+            
+        }
+        
+        dir = new File("c:/loja/site");
+        
+        if (!dir.exists()) {
+            
+            dir.mkdir();
+            
+        }
+        
+        dir = new File("c:/loja/site/imagens");
+        
+        if (!dir.exists()) {
+            
+            dir.mkdir();
+            
+        }
+        
+        dir = new File("c:/loja/site/videos");
+        
+        if (!dir.exists()) {
+            
+            dir.mkdir();
+            
+        }
+        
+        dir = new File("c:/loja/site/sons");
+        
+        if (!dir.exists()) {
+            
+            dir.mkdir();
+            
+        }
+        
+        System.out.println("Final do processo de criação");
+        
+    }
+    
+}
